4592a56b43f2fd421fc455748016820d1e4ded96
Shatila Shahi <shatila02@student.sust.edu>
2022-11-01 05:25:30 +0600

    First Error 500

--- projectweb/settings.py
+++ projectweb/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(*n-v)d_=m=1pgp$!31b6@=)@uc0o_ja*a9ic)pmq3^=wk_g+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
