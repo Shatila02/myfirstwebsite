1e089e4bd2afa3f3d252ad442b245611839b2bf6
Shatila Shahi <shatila02@student.sust.edu>
2022-11-06 04:52:29 +0600

    debugging in settings

--- projectweb/settings.py
+++ projectweb/settings.py
@@ -96,7 +96,9 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'.update(db_from_env)]
+DATABASES['default'].update(db_from_env)
+
+
 AUTHENTICATION_BACKENDS = [
     
     # Needed to login by username in Django admin, regardless of `allauth`
